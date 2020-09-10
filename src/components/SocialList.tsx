9d39d7afbd331596fe307d36e11e7f3b9b4afb16
Tat Tran <tattran22@outlook.com>
2020-09-11 00:45:42 +0700

    remove Twitter link

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -15,14 +15,14 @@ export function SocialList({}) {
       >
         <Facebook width={24} height={24} fill={'#222'} />
       </a>
-      <a
+      {/* <a
         title='Twitter'
         href={`https://twitter.com/${config.twitter_account}`}
         target='_blank'
         rel='noopener'
       >
         <Twitter width={24} height={24} fill={'#222'} />
-      </a>
+      </a> */}
       <a
         title='GitHub'
         href={`https://github.com/${config.github_account}`}
